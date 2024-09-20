33603fe1a43cb7463f316898b8a691ee439e20cf
Berkin Güler <berkinguler@live.com>
2024-09-20 16:24:42 +0300

    elevenlabs use async client with streaming mode

--- src/pipecat/services/elevenlabs.py
+++ src/pipecat/services/elevenlabs.py
@@ -4,30 +4,20 @@
 # SPDX-License-Identifier: BSD 2-Clause License
 #
 
-import asyncio
-import base64
-import json
+from typing import AsyncGenerator, Literal, Optional
 
-from typing import Any, AsyncGenerator, List, Literal, Mapping, Tuple, Optional
+from elevenlabs import VoiceSettings
 from pydantic import BaseModel
 
-from pipecat.frames.frames import (
-    AudioRawFrame,
-    CancelFrame,
-    EndFrame,
-    Frame,
-    StartFrame,
-    StartInterruptionFrame,
-    TTSStartedFrame,
-    TTSStoppedFrame)
-from pipecat.processors.frame_processor import FrameDirection
-from pipecat.services.ai_services import AsyncWordTTSService
+from pipecat.frames.frames import AudioRawFrame, Frame, TTSStartedFrame, TTSStoppedFrame
+from pipecat.services.ai_services import TTSService
 
 from loguru import logger
 
 # See .env.example for ElevenLabs configuration needed
 try:
-    import websockets
+    from elevenlabs.client import AsyncElevenLabs
+
 except ModuleNotFoundError as e:
     logger.error(f"Exception: {e}")
     logger.error(
@@ -47,45 +37,7 @@ def sample_rate_from_output_format(output_format: str) -> int:
             return 44100
     return 16000
 
-
-def calculate_word_times(
-        alignment_info: Mapping[str, Any], cumulative_time: float
-) -> List[Tuple[str, float]]:
-    zipped_times = list(zip(alignment_info["chars"], alignment_info["charStartTimesMs"]))
-
-    words = "".join(alignment_info["chars"]).split(" ")
-
-    # Calculate start time for each word. We do this by finding a space character
-    # and using the previous word time, also taking into account there might not
-    # be a space at the end.
-    times = []
-    for (i, (a, b)) in enumerate(zipped_times):
-        if a == " " or i == len(zipped_times) - 1:
-            t = cumulative_time + (zipped_times[i - 1][1] / 1000.0)
-            times.append(t)
-
-    word_times = list(zip(words, times))
-
-    return word_times
-
-class VoiceSettings:
-    stability: Optional[float] = None
-    similarity_boost: Optional[float] = None
-    style: Optional[float] = None
-    use_speaker_boost: Optional[bool] = None
-
-    def to_dict(self) -> dict:
-        return {
-            key: value for key, value in {
-                "stability": self.stability,
-                "similarity_boost": self.similarity_boost,
-                "style": self.style,
-                "use_speaker_boost": self.use_speaker_boost
-            }.items() if value is not None
-        }
-
-
-class ElevenLabsTTSService(AsyncWordTTSService):
+class ElevenLabsTTSService(TTSService):
     class InputParams(BaseModel):
         output_format: Literal["pcm_16000", "pcm_22050", "pcm_24000", "pcm_44100"] = "pcm_16000"
 
@@ -94,49 +46,20 @@ class ElevenLabsTTSService(AsyncWordTTSService):
             *,
             api_key: str,
             voice_id: str,
-            voice_settings: Optional[VoiceSettings] = None,
             model: str = "eleven_turbo_v2_5",
-            url: str = "wss://api.elevenlabs.io",
+            voice_settings: Optional[VoiceSettings] = None,
             params: InputParams = InputParams(),
             optimize_streaming_latency: int = 0,
             **kwargs):
-        # Aggregating sentences still gives cleaner-sounding results and fewer
-        # artifacts than streaming one word at a time. On average, waiting for a
-        # full sentence should only "cost" us 15ms or so with GPT-4o or a Llama
-        # 3 model, and it's worth it for the better audio quality.
-        #
-        # We also don't want to automatically push LLM response text frames,
-        # because the context aggregators will add them to the LLM context even
-        # if we're interrupted. ElevenLabs gives us word-by-word timestamps. We
-        # can use those to generate text frames ourselves aligned with the
-        # playout timing of the audio!
-        #
-        # Finally, ElevenLabs doesn't provide information on when the bot stops
-        # speaking for a while, so we want the parent class to send TTSStopFrame
-        # after a short period not receiving any audio.
-        super().__init__(
-            aggregate_sentences=True,
-            push_text_frames=False,
-            push_stop_frames=True,
-            stop_frame_timeout_s=2.0,
-            **kwargs
-        )
+        super().__init__(**kwargs)
 
-        self._api_key = api_key
         self._voice_id = voice_id
-        self._voice_settings = voice_settings
         self._model = model
-        self._url = url
         self._params = params
-        self._sample_rate = sample_rate_from_output_format(params.output_format)
+        self._voice_settings = voice_settings
+        self._client = AsyncElevenLabs(api_key=api_key)
         self._optimize_streaming_latency = optimize_streaming_latency
-
-        # Websocket connection to ElevenLabs.
-        self._websocket = None
-        # Indicates if we have sent TTSStartedFrame. It will reset to False when
-        # there's an interruption or TTSStoppedFrame.
-        self._started = False
-        self._cumulative_time = 0
+        self._sample_rate = sample_rate_from_output_format(params.output_format)
 
     def can_generate_metrics(self) -> bool:
         return True
@@ -144,143 +67,36 @@ class ElevenLabsTTSService(AsyncWordTTSService):
     async def set_model(self, model: str):
         logger.debug(f"Switching TTS model to: [{model}]")
         self._model = model
-        await self._disconnect()
-        await self._connect()
 
     async def set_voice(self, voice: str):
         logger.debug(f"Switching TTS voice to: [{voice}]")
         self._voice_id = voice
-        await self._disconnect()
-        await self._connect()
-
-    async def start(self, frame: StartFrame):
-        await super().start(frame)
-        await self._connect()
-
-    async def stop(self, frame: EndFrame):
-        await super().stop(frame)
-        await self._disconnect()
-
-    async def cancel(self, frame: CancelFrame):
-        await super().cancel(frame)
-        await self._disconnect()
-
-    async def flush_audio(self):
-        if self._websocket:
-            msg = {"text": " ", "flush": True}
-            await self._websocket.send(json.dumps(msg))
-
-    async def push_frame(self, frame: Frame, direction: FrameDirection = FrameDirection.DOWNSTREAM):
-        await super().push_frame(frame, direction)
-        if isinstance(frame, (TTSStoppedFrame, StartInterruptionFrame)):
-            self._started = False
-            if isinstance(frame, TTSStoppedFrame):
-                await self.add_word_timestamps([("LLMFullResponseEndFrame", 0)])
-
-    async def _connect(self):
-        try:
-            voice_id = self._voice_id
-            model = self._model
-            output_format = self._params.output_format
-            optimize_streaming_latency = self._optimize_streaming_latency
-            url = f"{self._url}/v1/text-to-speech/{voice_id}/stream-input?model_id={model}&output_format={output_format}&optimize_streaming_latency={optimize_streaming_latency}"
-            self._websocket = await websockets.connect(url)
-            self._receive_task = self.get_event_loop().create_task(self._receive_task_handler())
-            self._keepalive_task = self.get_event_loop().create_task(self._keepalive_task_handler())
-
-            # According to ElevenLabs, we should always start with a single space.
-            msg = {
-                "text": " ",
-                "xi_api_key": self._api_key,
-            }
-            if self._voice_settings:
-                msg["voice_settings"] = self._voice_settings.to_dict()
-            await self._websocket.send(json.dumps(msg))
-        except Exception as e:
-            logger.error(f"{self} initialization error: {e}")
-            self._websocket = None
-
-    async def _disconnect(self):
-        try:
-            await self.stop_all_metrics()
-
-            if self._websocket:
-                await self._websocket.send(json.dumps({"text": ""}))
-                await self._websocket.close()
-                self._websocket = None
-
-            if self._receive_task:
-                self._receive_task.cancel()
-                await self._receive_task
-                self._receive_task = None
-
-            if self._keepalive_task:
-                self._keepalive_task.cancel()
-                await self._keepalive_task
-                self._keepalive_task = None
-
-            self._started = False
-        except Exception as e:
-            logger.error(f"{self} error closing websocket: {e}")
-
-    async def _receive_task_handler(self):
-        try:
-            async for message in self._websocket:
-                msg = json.loads(message)
-                if msg.get("audio"):
-                    await self.stop_ttfb_metrics()
-                    self.start_word_timestamps()
-
-                    audio = base64.b64decode(msg["audio"])
-                    frame = AudioRawFrame(audio, self._sample_rate, 1)
-                    await self.push_frame(frame)
-
-                if msg.get("alignment"):
-                    word_times = calculate_word_times(msg["alignment"], self._cumulative_time)
-                    await self.add_word_timestamps(word_times)
-                    self._cumulative_time = word_times[-1][1]
-        except asyncio.CancelledError:
-            pass
-        except Exception as e:
-            logger.error(f"{self} exception: {e}")
-
-    async def _keepalive_task_handler(self):
-        while True:
-            try:
-                await asyncio.sleep(10)
-                await self._send_text("")
-            except asyncio.CancelledError:
-                break
-            except Exception as e:
-                logger.error(f"{self} exception: {e}")
-
-    async def _send_text(self, text: str):
-        if self._websocket:
-            msg = {"text": text + " "}
-            await self._websocket.send(json.dumps(msg))
 
     async def run_tts(self, text: str) -> AsyncGenerator[Frame, None]:
         logger.debug(f"Generating TTS: [{text}]")
 
-        try:
-            if not self._websocket:
-                await self._connect()
+        await self.start_tts_usage_metrics(text)
+        await self.start_ttfb_metrics()
 
-            try:
-                if not self._started:
-                    await self.push_frame(TTSStartedFrame())
-                    await self.start_ttfb_metrics()
-                    self._started = True
-                    self._cumulative_time = 0
+        results = await self._client.generate(
+            text=text,
+            voice=self._voice_id,
+            model=self._model,
+            output_format=self._params.output_format,
+            voice_settings=self._voice_settings,
+            optimize_streaming_latency=self._optimize_streaming_latency,
+            stream=True
+        )
 
-                await self._send_text(text)
-                await self.start_tts_usage_metrics(text)
-            except Exception as e:
-                logger.error(f"{self} error sending message: {e}")
-                await self.push_frame(TTSStoppedFrame())
-                await self._disconnect()
-                await self._connect()
-                return
-            yield None
-        except Exception as e:
-            logger.error(f"{self} exception: {e}")
+        tts_started = False
+        async for audio in results:
+            # This is so we send TTSStartedFrame when we have the first audio
+            # bytes.
+            if not tts_started:
+                await self.push_frame(TTSStartedFrame())
+                tts_started = True
+            await self.stop_ttfb_metrics()
+            frame = AudioRawFrame(audio, self._sample_rate, 1)
+            yield frame
+
+        await self.push_frame(TTSStoppedFrame())
